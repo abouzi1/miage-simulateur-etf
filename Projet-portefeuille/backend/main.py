706de77959110d1d9ec32534ac1763b91e35a888
olympiagks3 <olympiageorgiakakis@gmail.com>
2026-04-27 13:37:12 +0200

    Update main.py

--- Projet-portefeuille/backend/main.py
+++ Projet-portefeuille/backend/main.py
@@ -1,23 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# Initialisation de l'application
-app = FastAPI(
-    title="API Portefeuille ETF",
-    description="Serveur Backend pour le projet de simulation d'investissement",
-    version="1.0.0"
-)
+app = FastAPI(title="Simulateur Portefeuille Passif")
 
-# Configuration CORS (Indispensable pour que React puisse communiquer avec Python)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # En phase de développement, on autorise toutes les adresses
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Création d'une route de test basique
 @app.get("/")
-def read_root():
-    return {"message": "Le serveur Backend est en ligne et prêt à envoyer des données !"}
+def root():
+    return {"status": "ok"}
